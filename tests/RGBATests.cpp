fc4af20aa54e0c87dd6c52639be77b0fecc3bd5f
Marshall Chan <marshallchan666@outlook.com>
2023-10-05 17:09:59 +0100

    fix: fix the issue on my mac system,change slightly on cmake configuration

--- tests/RGBATests.cpp
+++ tests/RGBATests.cpp
@@ -1,11 +1,10 @@
-#include <gtest/gtest.h>
 #include "RGBA.h"
+#include <gtest/gtest.h>
 
-TEST(RGBA,construct) 
-{
-    RGBA pixel;
-    EXPECT_EQ(pixel.r,0); 
-    EXPECT_EQ(pixel.g,0);
-    EXPECT_EQ(pixel.b,0);
-    EXPECT_EQ(pixel.a,255);
+TEST(RGBA, construct) {
+  RGBA pixel;
+  EXPECT_EQ(pixel.r, 0);
+  EXPECT_EQ(pixel.g, 0);
+  EXPECT_EQ(pixel.b, 0);
+  EXPECT_EQ(pixel.a, 255);
 }
